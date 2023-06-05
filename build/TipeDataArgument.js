7247819ac815c18c74b70e90894d486bec6ac5e9
narwinoks <narnowin195@gmail.com>
2023-06-05 21:48:01 +0700

    fix function

---
+++ build/TipeDataArgument.js
@@ -0,0 +1,6 @@
+"use strict";
+console.log("Tipe data argument");
+function multiply(a, b) {
+    return a * b;
+}
+console.log(multiply(2, 2));
---
+++ src/TipeDataArgument.ts
@@ -0,0 +1,7 @@
+console.log("Tipe data argument");
+
+function multiply(a: number, b: number): number {
+    return a * b;
+}
+
+console.log(multiply(2, 2));
