216684effd5b9d503c7f7d1672553ee1b01eddba
narwinoks <narnowin195@gmail.com>
2023-06-05 21:56:56 +0700

    fix tipe data function

---
+++ build/TipeDataFunction.js
@@ -0,0 +1,6 @@
+"use strict";
+console.log("tipe data function");
+const agee = 12;
+const add = (a, b) => {
+    return a + b;
+};
---
+++ src/TipeDataFunction.ts
@@ -0,0 +1,10 @@
+console.log("tipe data function");
+ type Age =Number;
+ const agee :Age =12
+
+//  function type
+type Tambah =(a:number , b:number) => number;
+
+const add :Tambah =(a:number , b:number) :number => {
+    return a + b;
+}
