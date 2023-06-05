9cdd7975d4db58189a7d6188ec900cbd70ca94c9
narwinoks <narnowin195@gmail.com>
2023-06-05 21:13:30 +0700

    tipe data

---
+++ build/typeDasar.js
@@ -0,0 +1,23 @@
+"use strict";
+// tipe data string
+let nama = "name";
+nama = "20";
+nama = "update value nama";
+console.log("tipe data string = " + nama);
+// tipe data number
+let age = 20;
+age = 200;
+console.log("tipe data number = " + age);
+// tipe data boolean
+let state = false;
+state = true;
+state = false;
+console.log("tipe data boolean = " + state);
+// tipe data any 
+let hero = "boolean";
+hero = true;
+hero = false;
+hero = 20;
+hero = {};
+hero = [];
+console.log("tipe data any = " + hero);
---
+++ src/typeDasar.ts
@@ -0,0 +1,28 @@
+// tipe data string
+let nama :string ="name";
+nama ="20";
+nama ="update value nama";
+console.log("tipe data string = " + nama);
+
+
+// tipe data number
+let age :number =20;
+age =200;
+console.log("tipe data number = " + age);
+
+// tipe data boolean
+let state :boolean =false;
+state =true;
+state =false;
+console.log("tipe data boolean = " + state);
+
+// tipe data any 
+let hero :any ="boolean"
+hero =true;
+hero =false;
+hero =20;
+hero ={};
+hero = [];
+console.log("tipe data any = " + hero);
+
+
