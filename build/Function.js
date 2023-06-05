d02cc2f048f79988fc80d71311f604375910fd35
narwinoks <narnowin195@gmail.com>
2023-06-05 21:42:26 +0700

    fix function

---
+++ build/Function.js
@@ -0,0 +1,17 @@
+"use strict";
+// function
+// tipe data balikan function
+function getName() {
+    return "hello my name world!";
+}
+console.log(getName());
+function getAge() {
+    return 20;
+}
+console.log(getAge());
+function getAddress() {
+    return "";
+}
+function printName() {
+    console.log("void not returned");
+}
---
+++ src/Function.ts
@@ -0,0 +1,23 @@
+// function
+// tipe data balikan function
+
+function getName (): string {
+    return "hello my name world!";
+}
+
+console.log(getName());
+
+function getAge (): number {
+    return 20;
+}
+
+console.log(getAge());
+
+function getAddress (): string {
+    return ""
+}
+
+function printName (): void {
+    console.log("void not returned");
+    
+}
