1025438f406949209db4b37bd37ac02b314ed124
narwinoks <narnowin195@gmail.com>
2023-06-05 21:23:01 +0700

    fix array tipe data

---
+++ build/array.js
@@ -0,0 +1,16 @@
+"use strict";
+// array 
+let array;
+array = [12, 13, 14, 15, 16];
+console.log("array tipe number" + array);
+// array tipe string
+let array2;
+array2 = ['a', 'b', 'c', 'd', 'e'];
+console.log("array tipe string" + array2);
+// any array
+let array3;
+array3 = ['a', 'b', 'c', 'd', 'e', 13, true];
+console.log("array tipe any" + array3);
+// tuples
+let tuples;
+tuples = ['string', 1, true];
---
+++ src/array.ts
@@ -0,0 +1,18 @@
+// array 
+let array :number[];
+array =[12, 13, 14, 15, 16]
+console.log("array tipe number" + array);
+// array tipe string
+let array2 :string[] ;
+array2 =['a', 'b', 'c', 'd', 'e'];
+console.log("array tipe string" + array2);
+
+// any array
+let array3 :any[] ;
+array3 =['a', 'b', 'c', 'd', 'e',13,true] ;
+console.log("array tipe any" + array3);
+
+// tuples
+let tuples :[string,number,boolean] ;
+tuples =[ 'string',1,true] ;
+
